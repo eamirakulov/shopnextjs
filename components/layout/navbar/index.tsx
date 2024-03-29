460f78796deee8433ba0fa3c12b35e0b01909c13
emilopascope <emil@amirakulov.com>
2024-03-29 10:39:01 +0600

    Changed container padding/width

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -21,49 +21,51 @@ export default async function Navbar() {
     }
   ];
   return (
-    <nav className="relative flex items-center justify-between border-b border-[#f5f5f5] p-4 shadow-sm lg:p-6">
-      <div className="block flex-none md:hidden">
-        <MobileMenu menu={menu} />
-      </div>
-      <div className="flex w-full items-center justify-between">
-        <div className="flex w-full gap-4 md:w-1/3">
-          <Link
-            href="/"
-            aria-label="Go back home"
-            className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
-          >
-            <Image
-              src={logo}
-              alt="logo"
-              className="main-logo xs:max-w-[150px] md:max-w-[200px]"
-              priority={true}
-            />
-          </Link>
-          {menu.length ? (
-            <ul className="text-md hidden gap-8 md:flex md:items-center">
-              {menu.map((item: Menu) => (
-                <li key={item.title}>
-                  <Link
-                    href={item.path}
-                    className="text-neutral-500 underline underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : null}
+    <div className="container">
+      <nav className="relative flex items-center justify-between border-b border-[#f5f5f5] pt-6 pb-6 shadow-sm">
+        <div className="block flex-none md:hidden">
+          <MobileMenu menu={menu} />
         </div>
-        {/* <div className="hidden justify-center md:flex md:w-1/3">
-          <Search />
-        </div> */}
-        <div className="flex justify-end md:w-1/3">
-          {/* <OpenCart /> */}
-          <Suspense fallback={<OpenCart />}>
-            <Cart />
-          </Suspense>
+        <div className="flex w-full items-center justify-between">
+          <div className="flex w-full gap-4 md:w-1/3">
+            <Link
+              href="/"
+              aria-label="Go back home"
+              className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
+            >
+              <Image
+                src={logo}
+                alt="logo"
+                className="main-logo xs:max-w-[150px] md:max-w-[200px]"
+                priority={true}
+              />
+            </Link>
+            {menu.length ? (
+              <ul className="text-md hidden gap-8 md:flex md:items-center">
+                {menu.map((item: Menu) => (
+                  <li key={item.title}>
+                    <Link
+                      href={item.path}
+                      className="text-neutral-500 underline underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+          {/* <div className="hidden justify-center md:flex md:w-1/3">
+            <Search />
+          </div> */}
+          <div className="flex justify-end md:w-1/3">
+            {/* <OpenCart /> */}
+            <Suspense fallback={<OpenCart />}>
+              <Cart />
+            </Suspense>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
